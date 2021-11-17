97d797a16e43035a0a8da24b91ee3d2419c559b9
Remmelt Pit <remmelt@hipcode.io>
2021-11-17 21:37:32 +0100

    gitignore_global

--- makesymlinks.sh
+++ makesymlinks.sh
@@ -1,6 +1,6 @@
 #! /bin/sh
 
-files="gitconfig vimrc zshrc bin p10k.zsh zsh_options"
+files="gitconfig gitignore_global vimrc zshrc bin p10k.zsh zsh_options"
 
 for file in $files; do
   ln -s "$PWD"/$file ~/.$file
